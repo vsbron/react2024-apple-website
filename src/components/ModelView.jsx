6aacc715c10da8852080a7a69a9c736ff9d8e220
vsbron <vsbron@gmail.com>
2024-08-08 12:18:17 +0300

    Added iPhone model to interact with

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,6 +1,63 @@
-function ModelView() {
+import { Suspense } from "react";
+import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei";
+import * as THREE from "three";
+
+import IPhone from "./IPhone";
+import Lights from "./Lights";
+
+function ModelView({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationState,
+  item,
+  size,
+}) {
+  //
+
   // Returned JSX
-  return <div>Model View</div>;
+  return (
+    <View
+      index={index}
+      id={gsapType}
+      className={`w-full h-full ${index === 2 && "right-[-100%]"}`}
+    >
+      {/* Ambient lighting */}
+      <ambientLight intensity={0.3} />
+
+      {/* Perspective Camera */}
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      {/* Additional lights */}
+      <Lights />
+
+      {/* iPhone model under Suspense component */}
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthAngle())}
+      />
+
+      <group
+        ref={groupRef}
+        name={`${index === 1 ? "small" : "large"}`}
+        position={[0, 0, 0]}
+      >
+        <Suspense fallback={<div>Loading...</div>}>
+          <IPhone
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
+    </View>
+  );
 }
 
 export default ModelView;
